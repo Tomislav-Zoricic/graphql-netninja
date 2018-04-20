11cfaafa910973cf80296558d26775bea62d56e5
Tomislav-Zoricic <tomislav.zoricic.2@gmail.com>
2018-04-20 23:39:19 +0200

    Displaying book with author, routing between books

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import BookList from './components/BookList';
 import Book from './components/BookDetails';
-import AddBook from './components/AddBook';
 
 // apollo client setup
 const client = new ApolloClient({
--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { graphql } from 'react-apollo';
 import { bookQueries } from '../queries';
+import './Book.css';
 
 class BookDetails extends Component {
+  constructor(props) {
+    super();
+    this.renderAuthorBooks = this.renderAuthorBooks.bind(this);
+  }
+
+  renderAuthorBooks(author) {
+    return author.books.map(book => {
+        return <li key={book.id}>
+          <Link to={`/book/${book.id}`}>{book.name}</Link>
+        </li>
+    });
+  }
+
   render() {
     const data = this.props.data;
     if (data.loading) {
@@ -11,8 +26,11 @@ class BookDetails extends Component {
 
     return (
       <div>
-        <li>Book Name: {data.book.name}</li>
-        <li>Book Genre: {data.book.genre}</li>
+        <p>Book Name: {data.book.name}</p>
+        <p>Book Genre: {data.book.genre}</p>
+        <p>Author Name: {data.book.author.name}</p>
+        <p>Author Age: {data.book.author.age}</p>
+        {this.renderAuthorBooks(data.book.author)}
       </div>
     )
   }
@@ -27,4 +45,5 @@ const queryOptions = {
   })
 }
 
-export default graphql(bookQueries.getBookQuery, queryOptions)(BookDetails);
+export default
+ graphql(bookQueries.getBookQuery, queryOptions)(BookDetails);
